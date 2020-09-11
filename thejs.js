3e8ff7e231946abf34396ae4bc55d51537cf5ee5
Waylon Eklund <waylon.eklund@centre.edu>
2020-09-11 11:41:48 -0400

    Interactive

--- thejs.js
+++ thejs.js
@@ -1,1 +1,40 @@
+d3.select("#h1")
+    .on("click", function()
+{
+    d3.select("#h1")
+        .text("This is a header");
+})
+d3.select("#h1")
+    .on("mouseleave", function()
+{
+    d3.select("#h1")
+        .text("THIS IS NOT A HEADER");
+})
 
+d3.select("#img1")
+    .on("click", function()
+{
+    d3.select("#img1")
+    .attr("src","images/Peacock.jpg");
+})
+
+d3.select("#img1")
+    .on("mouseleave", function()
+{
+    d3.select("#img1")
+    .attr("src","images/Glass-Bottle.jpg");
+})
+
+d3.select("#img2")
+    .on("click", function()
+{
+    d3.select("#img2")
+    .attr("src","images/Peacock.jpg");
+})
+
+d3.select("#img2")
+    .on("mouseleave", function()
+{
+    d3.select("#img2")
+    .attr("src","images/The-Girl.jpg");
+})
